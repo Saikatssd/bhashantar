081699938f9b971423c294577c7ac7c47be0ef84
Saikat Sundar Das <saikatsundardas@gmail.com>
2024-06-27 14:41:14 +0530

    companyName changed to company_name

--- server/models/CompanySchema.js
+++ server/models/CompanySchema.js
@@ -4,6 +4,7 @@ const CompanySchema = new mongoose.Schema({
   name: { type: String, required: true },
   admin: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
   user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  
 });
 
 module.exports = mongoose.model('Company', CompanySchema);
